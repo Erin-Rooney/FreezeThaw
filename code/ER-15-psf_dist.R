b228cbd63a3a557061d21c6f0059f6642114879d
Erin-Rooney <61806923+Erin-Rooney@users.noreply.github.com>
2020-09-15 15:33:29 -0700

    Update ER-15-psf_dist.R

--- code/ER-15-psf_dist.R
+++ code/ER-15-psf_dist.R
@@ -7,17 +7,47 @@ plot(breadthdata_csv)
 rep_1 = breadthdata_csv[breadthdata_csv$sample=="40_50_16",]
 
 tool = breadthdata_csv[breadthdata_csv$site=="tool,"]
+breadth_freq = breadthdata_csv$"breadth_freq"
+trmt = breadthdata_csv$"trmt"
+bin = breadthdata_csv$"bin"
+
 
 ################
 
-breadth_aov1 = aov("breadth_dist" ~ "trmt" * "bin", data = tool)
-summary(breadth_aov1)
+#breadth_aov1 = aov("breadth_dist" ~ "trmt" * "bin", data = tool)
+#summary(breadth_aov1)
 
 
+breadth.aov1 <- aov(breadth_freq ~ trmt, data = tool)
+summary.aov(breadth.aov1)
+
+
+breadth.aov1 <- aov(breadth_freq ~ trmt * bin, data = tool)
+summary.aov(breadth.aov1)
+
+breadth.aov1 <- aov(breadth_freq ~ bin + trmt, data = tool)
+summary.aov(breadth.aov1)
 
 ################
 
 library(ggplot2)
+
+################
+
+p = ggplot(rep_1, aes(x = bin, y=breadth_freq, color = trmt))+
+  geom_boxplot()+
+  #geom_density(adjust=0.5)+
+  
+  labs (title = "Impact of Freeze/Thaw Cycles on Pore Size Distribution",
+        subtitle = "40-50 cm, 16% moisture",
+        caption = "Permafrost Soil Aggregate from Toolik, Alaska",
+        tag = "Figure 1",
+        x = expression (bold ("Pore Throat Diameter, um")),
+        y = expression (bold ("Distribution, %")))
+
+p + scale_color_manual(values=c("#00FFFF", "#996633"))
+
+
 
 
 ##############
